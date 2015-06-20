862d8db0d7b81213f0c9c898c8a9f9b743638f99
Leonid Grinberg <lgrinberg@gmail.com>
2015-06-20 01:09:34 -0400

    Add support for default values.
    Default values can be indicated with a "dict"-based config of the columns. Something like this:
    
        ORM("...", {
            "table": "my_table",
            "columns": {
                "foo": { "type": int, "default" 5 }
            }
        })
    
    The old syntax (where column values were just the types) is still
    supported.
    
    Columns are not required to have a default value. If a default value
    is not provided, a value of "None" is assumed (irrespective of the
    type of the column). When an object is mapped, *ALL* of the columns
    will be present. Those that are not supplied with keyword arguments
    will have the default value if one is supplied, or None otherwise. For
    example:
    
      Person = ORM(":memory:", {
          "table": "people",
          "columns": {
             "first_name": str,
             "last_name": { "type": str, "default": "Doe" }
          }
      }
    
      john_doe = Person(first_name="John", last_name="Doe")
      jane_doe = Person(first_name="Jane")
      sam_doe = Person(first_name="Sam", age=20)
    
    All of the above will have the last name "Doe". john_doe.age and
    jane_doe.age will return None, while sam_doe.age will return 20.

--- orm.py
+++ orm.py
@@ -35,6 +35,20 @@ def ClassFactory(name, argument_typecasters, exceptions=[], BaseClass=object, **
     return newclass
 
 
+def NoneSafeType(_type):
+    """
+    A hack for a "None-safe" typecaster. Given a type, it casts all
+    values to that type as the type would, except that None is always
+    cast to None.
+    """
+    def caster(value):
+        if value is None:
+            return None
+        else:
+            return _type(value)
+    return caster
+
+
 class ORMBase(object):
     """
     This class serves as the base for each instance of a "mapped
@@ -82,25 +96,51 @@ class ORM(object):
             raise TypeError("Schema must contain table name (string).")
         if "columns" not in schema or type(schema["columns"]) is not dict:
             raise TypeError("Schema must contain column definitions (dict names -> types).")
-        if not set(schema["columns"].values()).issubset(set(ORM.known_column_types.keys())):
-            raise TypeError("Illegal column type! Must be one of %s." % (ORM.known_column_types.keys(),))
         if "id" in schema["columns"].keys():
             raise TypeError("Cannot use 'id' as column name. It's reserved.")
         self.connection = sqlite3.connect(database_name, check_same_thread=False)
         self.connection.row_factory = sqlite3.Row
         self.connection_lock = threading.RLock()
         self.table = schema["table"]
-        self.columns = copy.deepcopy(schema["columns"])
-        self.columns["id"] = int
+        self.columns = { "id": { "type": int } }
+        argument_typecasters = { "id": NoneSafeType(int) }
+        for column_name, column_definition in copy.deepcopy(schema["columns"]).items():
+            # long form definition
+            #    favorite_number: { "type": int, "default": 5 }
+            if type(column_definition) is dict:
+                if "type" not in column_definition:
+                    raise TypeError("Column definition %s must have 'type' parameter." % (column_definition,))
+                elif column_definition["type"] not in ORM.known_column_types:
+                    raise TypeError("Illegal column type! Must be one of %s." % (ORM.known_column_types.keys(),))
+                else:
+                    column_type = NoneSafeType(column_definition["type"])
+                    self.columns[column_name] = column_definition
+            # short form definition
+            #    favorite_number: int
+            elif type(column_definition) is type:
+                if column_definition not in ORM.known_column_types:
+                    raise TypeError("Illegal column type! Must be one of %s." % (ORM.known_column_types.keys(),))
+                column_type = NoneSafeType(column_definition)
+                self.columns[column_name] = { "type": column_definition }
+            argument_typecasters[column_name] = column_type
+        argument_typecasters["id"] = NoneSafeType(int)
         self.sync() # Will raise TypeError if columns unsafe
         self.mapped_object = ClassFactory("%s_Mapper" % (self.table,),
-                                          argument_typecasters=self.columns,
+                                          argument_typecasters=argument_typecasters,
                                           exceptions=["_ORMBase__orm"], # This is a magic internal name of the
                                                                         # __orm object that BaseClass defines.
                                           BaseClass=ORMBase,
                                           orm=self)
 
     def __call__(self, **kwargs):
+        for column_name, column_definition in self.columns.items():
+            if column_name == "id":
+                continue
+            elif column_name not in kwargs:
+                if "default" in column_definition:
+                    kwargs[column_name] = column_definition["default"]
+                else:
+                    kwargs[column_name] = None
         return self.mapped_object(**kwargs)
 
     def execute_query(self, query, params=[]):
@@ -120,34 +160,62 @@ class ORM(object):
               adds the additional columns.
             - If table exists and contains columns not in the given columns, prints a warning
               and ignores the extra columns.
-            - If table exists and contains a column with the same name but different type as
-              one of the given ones, raises an exception.
+            - If table exists and contains a column with the same name but different definition
+              (type or default value) as one of the given ones, raises an exception.
         """
         rows = self.execute_query("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name=?",
                                   (self.table,))
         table_exists = (rows[0][0] == 1)
         if table_exists:
-            column_definition_pairs = map(lambda row: (row[1], row[2].upper()),
+            db_column_definitions = map(lambda row: dict(column_name=row[1],
+                                                         column_type=row[2].upper(),
+                                                         default_value=row[4]),
                                           self.execute_query("PRAGMA table_info(%s)" % (self.table,)))
-            for (column_name, column_type) in column_definition_pairs:
-                if column_name not in self.columns:
-                    print "Column %s present in table but not in schema. We'll ignore it, so it won't be visible in the ORM!" % (column_name,)
+            for db_column_definition in db_column_definitions:
+                db_column_name = db_column_definition["column_name"]
+                if db_column_name not in self.columns:
+                    print "Column %s present in table but not in schema. We'll ignore it, so it won't be visible in the ORM!" \
+                        % (db_column_name,)
                     continue
-                if column_type != ORM.known_column_types[self.columns[column_name]]:
-                    raise TypeError("Column %s present in table has different type from the one in schema!" % (column_name,))
+                db_column_type = db_column_definition["column_type"]
+                orm_column_type = ORM.known_column_types[self.columns[db_column_name]["type"]]
+                if db_column_type != orm_column_type:
+                    raise TypeError("Column %s present in table has different type (%r) from the one in schema (%r)!" \
+                                    % (db_column_name, db_column_type, orm_column_type))
+                db_default_value = db_column_definition["default_value"]
+                orm_default_value = self.columns[db_column_name].get("default", None)
+                if db_default_value != orm_default_value:
+                    raise TypeError("Column %s present in table has different default value (%r) from the one in schema (%r)" \
+                                    % (db_column_name, db_default_value, orm_default_value))
             # If we got here, it means none of the table columns are
             # of a different type than what's in our schema.
-            for column_name, column_type in self.columns.items():
-                if column_name in map(lambda (column_name, column_type): column_name, column_definition_pairs):
+            for orm_column_name, orm_column_definition in self.columns.items():
+                if orm_column_name in map(lambda db_column_definition: db_column_definition["column_name"],
+                                          db_column_definitions):
                     continue
                 else:
-                    self.execute_query("ALTER TABLE %s ADD COLUMN %s %s"
-                                       % (self.table, column_name, ORM.known_column_types[column_type]))
+                    orm_column_type = ORM.known_column_types[orm_column_definition["type"]]
+                    orm_default_value = orm_column_definition.get("default", None)
+                    query = "ALTER TABLE %s ADD COLUMN %s %s" % (self.table, orm_column_name, orm_column_type)
+                    if orm_default_value is not None:
+                        query += " DEFAULT %s" % (orm_default_value,)
+                    self.execute_query(query)
         else:
-            column_definition_pairs = [(column_name, ORM.known_column_types[column_type])
-                                       for (column_name, column_type) in self.columns.items()
-                                       if column_name != "id"]
-            column_definitions = ",".join(map(lambda (column_name, column_type): "%s %s" % (column_name, column_type), column_definition_pairs))
+            column_definition_chunks = []
+            for orm_column_name, orm_column_definition in self.columns.items():
+                if orm_column_name == "id":
+                    continue
+                orm_column_type = ORM.known_column_types[orm_column_definition["type"]]
+                orm_default_value = orm_column_definition.get("default", None)
+                column_definition_chunks.append((orm_column_name,
+                                                 orm_column_type,
+                                                 orm_default_value))
+            def column_definition_chunk_to_query_chunk((column_name, column_type, default_value)):
+                query_chunk = "%s %s" % (column_name, column_type)
+                if default_value is not None:
+                    query_chunk += " DEFAULT %s" % (default_value,)
+                return query_chunk
+            column_definitions = ",".join(map(column_definition_chunk_to_query_chunk, column_definition_chunks))
             self.execute_query("CREATE TABLE %s (id INTEGER PRIMARY KEY AUTOINCREMENT, %s)"
                             % (self.table, column_definitions))
 
@@ -155,8 +223,7 @@ class ORM(object):
         def row_to_object(row):
             kwargs = {}
             for column_name, row_value in zip(row.keys(), list(row)):
-                if row_value is not None:
-                    kwargs[column_name] = row_value
+                kwargs[column_name] = row_value
             return self.mapped_object(**kwargs)
         rows = self.execute_query("SELECT %s FROM %s" % (",".join(self.columns), self.table,))
         objects = map(row_to_object, rows)
